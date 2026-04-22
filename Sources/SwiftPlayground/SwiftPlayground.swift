97f7b56e52b63dcecf9d86d28a581932e8e78c75
oc-kami-paverova-26 <kami.paverova@student.onslow.school.nz>
2026-04-22 00:58:21 +0000

    Apr 22, 2026, 12:58 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,37 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello world!!!~")
-    }
+        //Printing to test the commmand
+        print("Hello world!!!")
+        //printing a sad, heartfell, goodbye
+        print("Bye")
+        /* I printed these to test out if the "print" works*/
+        //I tried to print my initials. Then, for fun, I printed a smile
+        print("K    K   PPPP           S")
+        print("K  K     P   P      SS    S")
+        print("K        PPPP              S")
+        print("K  K     P          SS    S")
+        print("K    K   P              S")
+
+        var number = 1
+        print(number)
+        number = 2
+        print(number)
+        number = 3
+        print(number, "Hii", number)
+        //my firstname put as a constant. I did this to test out the constant function
+        let firstName = "Kami"
+        print("My name is",firstName)
+        print("My name is")
+        print(firstName)
+
+        //creates a constant = stays the same
+        //let HELLO = true
+        //creates variable = changes
+        //var Hi = true
+        //changes the value
+        //Hi = false
+        //boolean = true/false
+        //double = 
+            }
 }
