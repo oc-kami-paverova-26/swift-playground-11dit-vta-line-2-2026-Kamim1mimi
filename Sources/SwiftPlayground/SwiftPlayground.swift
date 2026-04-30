583e5845866c88d2672c418c8c525a743053a826
oc-kami-paverova-26 <kami.paverova@student.onslow.school.nz>
2026-04-30 02:38:12 +0000

    sad

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,41 +4,10 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        //Printing to test the commmand
-        print("Hello world!!!")
-        //printing a sad, heartfell, goodbye
-        print("Bye")
-        /* I printed these to test out if the "print" works*/
-        //I tried to print my initials. Then, for fun, I printed a smile
-        print("K    K   PPPP           S")
-        print("K  K     P   P      SS    S")
-        print("K        PPPP              S")
-        print("K  K     P          SS    S")
-        print("K    K   P              S")
-
-        var number = 1
-        print(number)
-        number = 2
-        print(number)
-        number = 3
-        print(number, "Hii", number)
-        //my firstname put as a constant. I did this to test out the constant function
-        let firstName = "Kami"
-        print("My name is",firstName)
-        print("My name is")
-        print(firstName)
-
-        //creates a constant = stays the same
-        //let HELLO = true
-        //creates variable = changes
-        //var Hi = true
-        //changes the value
-        // string = 
-        //boolean = true/false
-        //integer = whole number, no decimals  (if in quotation marks, it's a string and it isn't able to calculate a string)
-        //Floating point numbers (double) = decimal (e.g. 1.0 0.5 1.001)
-        /*Calculating numbers =  var result = 8+3   print(result)  resul = result + 3   print(result)   print(result - result - result)
-        
-        % = even or not number*/
+            //if the nunmber is above 18, it's true. If not, it's false
+            let isOldEnough = 16>=18
+            print(isOldEnough)
+            print("How old are you?")
+            age = 
             }
 }
