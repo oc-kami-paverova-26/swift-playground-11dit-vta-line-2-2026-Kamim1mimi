a9b63e4afd03dc48d5b16d21a717301323112234
oc-kami-paverova-26 <kami.paverova@student.onslow.school.nz>
2026-05-03 22:29:20 +0000

    sad

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,10 +4,42 @@
 @main
 struct SwiftPlayground {
     static func main() {
+        
+        let a = 40
+        let b = 920
+        let c = a + b
+        print(a)
+
+
+
+        /*print("What is your age in numbers")
+        let userage = readLine()!
+        let age = Int(userage)!
+        let isOldEnough = age >= 18
+        print(isOldEnough)
+        print("Enter the password")
+        let password = readLine()!
+        let secretpassword = password == "hello"
+        let oldEnough = secretpassword && isOldEnough
+        print(oldEnough)*/
+
+        /* 
+        print("What's your age?")
+        var userInput = readLine()!
+        let age = Int(userInput)!
+        let isOldEnough = age >= 18
+        print(isOldEnough)
+        print("0 out of 10, how much do you like minecraft?")
+        */
+
+        /*
             //if the nunmber is above 18, it's true. If not, it's false
-            let isOldEnough = 16>=18
+            var isOldEnough = 16>=18
             print(isOldEnough)
             print("How old are you?")
-            age = 
+            var age = readLine()!
+            let
+            isOldEnough = age>=18
+            print(isOldEnough)*/
             }
 }
