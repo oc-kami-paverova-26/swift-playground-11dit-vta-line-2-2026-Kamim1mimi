b4947d61c771e7d2c5adbef181628881ac0e756b
oc-kami-paverova-26 <kami.paverova@student.onslow.school.nz>
2026-05-03 22:48:47 +0000

    booleans

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,12 +5,33 @@
 struct SwiftPlayground {
     static func main() {
         
-        let a = 40
-        let b = 920
+        //let a = 40
+        //let b = 920
+        print("Write 1st number")
+        let a1 = readLine()!
+        print("Write 2nd number")
+        let a = Int(a1)!
+        let b1 = readLine()!
+        let b = Int(b1)!
+        print("1st and 2nd combined into 3rd:")
         let c = a + b
-        print(a)
-
-
+        print(c)
+        print("Is the 3rd bigger than 2nd?")
+        let d = c>b
+        print(d)
+        print("Is 1st smaller than 2nd AND bigger than 20?")
+        let e = a<b && a>20
+        print(e)
+        print("Is 2nd even or 1st odd?")
+        let f = (b % 2) == 0 || (a % 2) != 0
+        print(f)
+        print("Is 3rd odd AND bigger than 50?")
+        let g = c % 2 != 0 || c >= 50
+        print(g)
+        print("Are they all true?")
+        let x = d && e && f && g
+        print(x)
+        
 
         /*print("What is your age in numbers")
         let userage = readLine()!
