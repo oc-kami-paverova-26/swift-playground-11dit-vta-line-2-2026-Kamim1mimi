95bcde58eb0fd20685a0538175172fe29c22c11a
oc-kami-paverova-26 <kami.paverova@student.onslow.school.nz>
2026-05-14 02:03:05 +0000

    ss

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,12 +4,56 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        let items: [String] = ["Pen", "Paper", "Book"]
-        let friends: [String] = ["James", "Xander", "me", "Myself", "and I", "And mi"]
-        items.forEach {
-        item in print(item)
-        print(items)
+        /*var count = 0
+        while count < 10 {
+            print("Beep, beep, \(count).")
+            count = count + 1*/
+
+        var isRunning = true
+        var ticket = 0
+        print("A = child, B = Adult, C = elderly, Q = quit.")
+        var input = readLine()!.lowercased()
+        while isRunning {switch input {
+            case "a":
+                ticket = ticket + 5
+                print("This will be \(ticket) dollars.")
+            case "b":
+                ticket = ticket + 8
+                print("This will be \(ticket) dollars.")
+            case "c":
+                ticket = ticket + 12
+                print("This will be \(ticket) dollars.")
+            case "q":
+                print("Your total is \(ticket) dollars.")
+                isRunning = false
+            default:
+                print("This key doesn't do anything.")
+                
+            var input = readLine()!.lowercased()
+        }
         }
 
-    }
+        /*print("Buy tickets here. Write:")
+        print("C if you're below 12 years old. Price: 5 dollars.")
+        print("A if you're above 12 but below 18. Price: 8 dollars.")
+        print("O if you're above 18. Price: 12 dollars.")
+        print("Q when you're done.")*/
+
+        /*while isRunning == true {
+            var userInput = readLine()!.lowercased()
+            if userInput == "c" {
+                ticket = ticket + 5
+            } else if userInput == "a"
+                { ticket = ticket + 8
+            } else if userInput == "o"
+                { ticket = ticket + 12
+            } else if userInput == "q"
+                { isRunning = false
+            }
+            print(ticket, "dollars.")
+        }
+        if isRunning == false {
+            print("Your total is \(ticket). Enjoy your movie.")}
+        */
+        }
 }
