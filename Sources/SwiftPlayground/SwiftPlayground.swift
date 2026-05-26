11f93153078074b64130844959cfc329aba4b87e
oc-kami-paverova-26 <kami.paverova@student.onslow.school.nz>
2026-05-26 21:08:45 +0000

    sss

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,12 +4,46 @@
 @main
 struct SwiftPlayground {
     static func main() {
+        //creates a variable that keeps the loop going. "?" wonder what that does
+        var age: Int? = nil
+        var innu = 0  //INvalid NUmber
+        //creates a loop while the loop variable is "nil"
+        while age == nil {
+            print("What's the required treshold?")
+            let min = Int(readLine()!)!
+            //asks the user to enter their age
+            print("Enter your age:")
+            //if it's possible to create 
+            if let input = readLine(), let enteredAge = Int(input) {
+                //looks at specific numbers
+                if enteredAge < 0 {
+                    print("Age cannot be negative. Enter a valid age.")
+                    innu = innu + 1
+                } else if enteredAge < min {
+                    print("You must be 18 or older. Enter a valid age.")
+                    innu = innu + 1
+                } else {
+                    //if the age is 18 or above, then they change the loop variable to that and end the loop
+                    age = enteredAge
+                }
+            }
+            else {
+                //if it's not a number
+                print("Invalid input. Enter a valid integer for age.")
+                innu = innu + 1
+            }}
+
+        //once a valid age is entered, they see this.
+        print("Valid age entered: \(age!)")
+        print("Amount of invalid inputs: \(innu)")
+        /*
         // Task - Only to accept people in if they are over the age of 18 
         // creates a variable that keeps the loop on
         var on = true
         //creates a loop incase the user inputs invalid input
         while on == true{
         //asks the user for age
+        print("New code")
         print("What is your age?")
         //if the user inputs a number
         if let input = readLine(), let age = Int(input) {
@@ -32,6 +66,6 @@ struct SwiftPlayground {
             print("Please enter a number.")
     }
         }
-
+*/
         }
 }
