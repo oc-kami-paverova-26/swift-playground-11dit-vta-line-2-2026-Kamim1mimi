ea418a92b67f65db1a55abb8bc66efcc4b270cf8
oc-kami-paverova-26 <kami.paverova@student.onslow.school.nz>
2026-05-27 22:33:32 +0000

    sss

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,68 +4,89 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        //creates a variable that keeps the loop going. "?" wonder what that does
-        var age: Int? = nil
-        var innu = 0  //INvalid NUmber
-        //creates a loop while the loop variable is "nil"
-        while age == nil {
-            print("What's the required treshold?")
-            let min = Int(readLine()!)!
-            //asks the user to enter their age
-            print("Enter your age:")
-            //if it's possible to create 
-            if let input = readLine(), let enteredAge = Int(input) {
-                //looks at specific numbers
-                if enteredAge < 0 {
-                    print("Age cannot be negative. Enter a valid age.")
-                    innu = innu + 1
-                } else if enteredAge < min {
-                    print("You must be 18 or older. Enter a valid age.")
-                    innu = innu + 1
-                } else {
-                    //if the age is 18 or above, then they change the loop variable to that and end the loop
-                    age = enteredAge
-                }
+        //the price of an item
+        let pricePerItem = 19.99
+        //how many items are bought
+        let quantity = 3.0
+        //how much the tax costs
+        let taxRate = 0.15
+        //how much has to go to the state
+        let tax = 1.15
+        // tells the users the price of the item
+        print("Price per item: $19.99")
+        //tells the user the price of all the items they have
+        print("Quantity: 3.0")
+        //tells how much it costs together
+        print("Subtotal: $\(pricePerItem * quantity)")
+        //tells how much is the taqx
+        print("Tax (15%): $\(pricePerItem * quantity * taxRate)")
+        //tells how much the item is with tax
+        print("Total: $\(pricePerItem * quantity * tax)")
+
+        let length = 12.5
+        let width = 8.00
+        let perimeterTime = 2.00
+        print("The length of the rectangle is \(length) units.")
+        print("The width of the rectangle is \(width) units.")
+        print("The area of the rectangle is \(length * width) square units.")
+        print("The perimeter of the rectangle is \(perimeterTime * (length + width)) units.")
+
+
+        //all variables/constantws
+        var on = true  //what keeps the program running
+        var checkOut = 0.00 //what it will cost
+        var amount = 0.00 //how many items
+        let milk = 0.99
+        let bread = 0.50
+        let eggs = 1.55
+        while on == true {
+        print("What will you buy today? We have:")
+        print("Milk $\(milk)")
+        print("Bread $\(bread)")
+        print("Eggs $\(eggs)")
+        print("Everything else was bought out, we apologize.")
+        print("So what will you buy? (Please mind your grammar)")
+        var willBuy = readLine()!.lowercased()
+        print("How many?")
+        amount = Double(readLine()!)!
+
+        if willBuy == "bread" {
+            print("How many loaves")
+            checkOut = checkOut + bread }
+        if willBuy == "eggs" {
+            print("How many?")
+            if amount > 0 {
+                print("\(amount) of eggs added to the cart.")
+                checkOut = checkOut + amount * eggs
+                print("Your total is \(checkOut)")
             }
             else {
-                //if it's not a number
-                print("Invalid input. Enter a valid integer for age.")
-                innu = innu + 1
-            }}
-
-        //once a valid age is entered, they see this.
-        print("Valid age entered: \(age!)")
-        print("Amount of invalid inputs: \(innu)")
-        /*
-        // Task - Only to accept people in if they are over the age of 18 
-        // creates a variable that keeps the loop on
-        var on = true
-        //creates a loop incase the user inputs invalid input
-        while on == true{
-        //asks the user for age
-        print("New code")
-        print("What is your age?")
-        //if the user inputs a number
-        if let input = readLine(), let age = Int(input) {
-            // doesn't accept invalid ages
-            if age <= 0 || age >= 122 {
-                print("Age incorrect.")
-            //doesn't accept ages 18 and below
-            } else if age <= 18 {
-                print("Too young.")
-                //stops the loop so they don't try to cheat the system
-                on = false
-            // if it's anything else (19 - 121), then they can enter
-            } else {
-                print("You may enter")
-                //so they don't have to repeatedly put in their answer
-                on = false
+                print("Please write a valid number.")
             }
-        } else {
-            //if the user doesn't input a number. No "on = false" because they need to input another input
-            print("Please enter a number.")
-    }
         }
-*/
+
+
         }
+
+
+        //the price of an item
+        let pricePerItem0 = readLine()
+        //how many items are bought
+        let quantity0 = readLine()
+        //how much the tax costs
+        let taxRate = 0.15
+        //how much has to go to the state
+        let tax = 1.15
+        // tells the users the price of the item
+        print("Price per item: $19.99")
+        //tells the user the price of all the items they have
+        print("Quantity: 3.0")
+        //tells how much it costs together
+        print("Subtotal: $\(pricePerItem * quantity)")
+        //tells how much is the taqx
+        print("Tax (15%): $\(pricePerItem * quantity * taxRate)")
+        //tells how much the item is with tax
+        print("Total: $\(pricePerItem * quantity * tax)")
+        
+            }
 }
