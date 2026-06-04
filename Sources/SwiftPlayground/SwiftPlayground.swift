ff0d9bcb5ad4da660efd0e6f2f3353786d4f2592
oc-kami-paverova-26 <kami.paverova@student.onslow.school.nz>
2026-06-04 02:43:08 +0000

    case

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,7 +4,7 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        /* doesn't work
+/* doesn't work
         print("Enter a random integer.")
         let ui = Int(readLine()!)
         if ui! >= 0 || ui! <= 0 {
@@ -12,8 +12,27 @@ struct SwiftPlayground {
         }
         else {
             print("You a bebe? Or you just can't read?")
-        } */
-        let age: Int = input(prompt: "What is your age?", from: 0, to: 100)
+} */
 
+/*
+        func input(prompt: String, from: Int, to: Int) {
+            while true {
+                print("Hello?")
+            }
+
+        }
+        let age: Int = input(prompt: "What is your age?", from: 0, to: 100)
+        */
+
+        print("What's your choice of a letter?")
+        let UI = Int(readLine()!)!
+
+        switch UI {
+            case 5: print("What?")
+            case ...100: print("Hii.")
+            case 101...1111: print("hi=eheheheh")
+            default: print("Hi?")
+            }  //what to do if none other are there
+        print("\(UI)? Really?")
         }        
         }
