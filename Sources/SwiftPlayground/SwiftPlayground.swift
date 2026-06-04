1496c706111be97733e7945b2b9bf842199ec70e
oc-kami-paverova-26 <kami.paverova@student.onslow.school.nz>
2026-06-04 02:31:32 +0000

    idk

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,8 +4,16 @@
 @main
 struct SwiftPlayground {
     static func main() {
-
+        /* doesn't work
+        print("Enter a random integer.")
+        let ui = Int(readLine()!)
+        if ui! >= 0 || ui! <= 0 {
+            print("Good job, you can listen!")
         }
-        
-        
+        else {
+            print("You a bebe? Or you just can't read?")
+        } */
+        let age: Int = input(prompt: "What is your age?", from: 0, to: 100)
+
+        }        
         }
