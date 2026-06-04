5a9f656a3347b2fb1e225d9179569c3a7427fe98
oc-kami-paverova-26 <kami.paverova@student.onslow.school.nz>
2026-06-04 02:27:42 +0000

    nothing happened

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -3,87 +3,31 @@
 
 @main
 struct SwiftPlayground {
-    static func main() {        /*func directionToTimeSquare() {
-            print("Walk 4 mins to 34th St Herald Square train station.")
-            print("Take the Northbound N, Q, R, or W train 1 stop.")
-            print("Get off the Times Square 42nd Street stop.")
-            print("Take lots of pictures! 📸")
-        }
-        directionToTimeSquare()*/
-        /*
-        func falseNumber() {
-            print("Hello")
-        }
-
-        var repeatNumberWeight = true
-        var repeatNumberHeight = true
-        while repeatNumberWeight == true {
-        print("Enter weight.")
-        if let weight = Double(readLine()!) {
-            repeatNumberWeight = false
-            let weight1 = weight
-        }
-        else {
-            falseNumber()
-        }
-        }
-        while repeatNumberHeight == true {
-        print("Enter height.")
-        if let height = Double(readLine()!) {
-            let height1 = height
-            repeatNumberHeight = false
-        }
-        else {
-            falseNumber()
-        }
-        }
-        }
-            func findBMI() {
-                //checks if both variables are numbers
-                if weight1 <= 0 || weight1 >= 0 && height1 <= 0 || height1 >= 0{
-                    let BMI = weight / height
-                    print("The BMI is:")
-                    print(BMI)
-                }
-                else {
-                    print("Please enter a valid number.")
-                }
+    static func main() {
+            func input(prompt: String, from: Int, to: Int) -> Int {
+                while true {
+                    print(prompt, terminator: " ")
+                    if let userInput = readLine(), let int = Int(userInput), (from...to).contains(int) {
+                        return int
+                    } else {
+                        print("You must enter a valid whole number (integer) from \(from) to \(to).")
+                    }
             }
-            findBMI()
-
-            let weight = 1.0
-            let height = 1.0
-            let bmi = weight / height
-            func findBMI() {
-                print("Your BMI is: \(bmi)")
-            }
-            findBMI()
+        }
 
 
+        let age: Int = input(prompt: "What is your age?", from: 0, to: 100)
 
-            print("Enter the first number (left-hand side):")
-            let leftHandSide = Int(readLine()!)!
-            print("Enter the second number (right-hand side): ")
-            let rightHandSide = Int(readLine()!)!
 
-            print("Type add or subtract: ")
-            let calculationType = readLine()!
+        switch age {
+        case 0: print("Hello, little baby!")
+        case ...17: print("You are too young.")
+        default: print("Welcome!")
+        }
 
-            func add() {
-            let answer = leftHandSide + rightHandSide
-            print(answer)
-            }
 
-            func subtract() {
-            let answer = leftHandSide - rightHandSide
-            print(answer)
-            }
 
-            if calculationType == "add"{
-            add()
-            } else if calculationType == "subtract"{
-            subtract()*/
-            }
+        }
         
         
         }
