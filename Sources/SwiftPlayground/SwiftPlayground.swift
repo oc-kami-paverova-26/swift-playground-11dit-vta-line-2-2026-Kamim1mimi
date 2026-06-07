cb2c7e1f2ee401444daa409b7525322bd8f5f107
oc-kami-paverova-26 <kami.paverova@student.onslow.school.nz>
2026-06-07 22:19:13 +0000

    nothing

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -24,15 +24,34 @@ struct SwiftPlayground {
         let age: Int = input(prompt: "What is your age?", from: 0, to: 100)
         */
 
-        print("What's your choice of a letter?")
-        let UI = Int(readLine()!)!
+        /*
+        print("What's your choice of a number?")
+        let UI = readLine()
+        let UI1 = readLine()
+        if let userInput = UI, let userInput1 = UI1 { //both needed to do this
+            print("hi \(userInput) \(userInput1)")
+        }*/
 
+        //what is the users age and name
+        print("Hello, welcome to BasicGame.")
+        print("Before we start, we have a few questions to ask.")
+        print("First, what is your name?")
+        let email: String? = readLine()
+
+        if let validEmail = email {
+            print("Email is \(validEmail)")
+        } else {
+            print("No email provided")
+        }
+
+        
+        /* WORKED
         switch UI {
             case 5: print("What?")
             case ...100: print("Hii.")
             case 101...1111: print("hi=eheheheh")
             default: print("Hi?")
             }  //what to do if none other are there
-        print("\(UI)? Really?")
+        print("\(UI)? Really?")*/
         }        
         }
