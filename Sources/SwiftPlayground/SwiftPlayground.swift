bccfa9b52819d1c5a3b536d3db3fb3d9b4e5abc0
oc-kami-paverova-26 <kami.paverova@student.onslow.school.nz>
2026-06-11 02:36:04 +0000

    start

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,8 +4,11 @@
 @main
 struct SwiftPlayground {
     static func main() {
-                
+        print("Enter a number between 1–9")
         
+        let userInput: Int = Int(readLine()!)!
+        if userInput >= userInput {}
         }
+
         }
         
