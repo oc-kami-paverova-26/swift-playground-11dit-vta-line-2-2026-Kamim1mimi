69458d70bf070087e581ab7f84dfccb48e52cd5d
oc-kami-paverova-26 <kami.paverova@student.onslow.school.nz>
2026-06-14 22:23:22 +0000

    not sure

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,48 @@
 @main
 struct SwiftPlayground {
     static func main() {
+        let birds: [String] = ["Tieke", "Kākā", "Takahē", "Hihi", "Kiwi", "Pāteke", "Tūī", "Kererū"]
+        let insects: [String] = ["Giant Wētā", "Tree Wētā", "Cave Wētā", "Putoko", "Pūngāwerewere", "Ngaokeoke", "Waemano", "Kapowai"]
+        var animalType = "none"
+        var animalNumber = 0
+        var loop = true
+
+        var animalsSeen: [String] = [""] 
+
+        print("Welcome to Zealandia Tracker.")
+        print("Did you see a bird or an insect")
+        while loop == true {
+            animalType = readLine()!.lowercased()
+            if animalType == "bird" {
+                loop = false
+                for (a, b) in birds.enumerated() {
+                    print(a, b)
+                    }
+                print("Enter a number.")
+                loop = true
+                while loop == true {
+                if let animalNumber = Int(readLine()!) {
+                    animalsSeen.append(birds[animalNumber])
+                } else {
+                    print("Please enter a valid integer.")
+                    }
+                }
+
+
+
+
+            if animalType == "insect" {
+                loop = false
+                for (a, b) in insects.enumerated() {
+                    print(a, b)
+                }
+            }
+            else {
+                print("Please enter a 'bird' or an 'insect'.")
+            }
+        }
+
+
         print("Enter a number between 1–9")
         
         let userInput: Int = Int(readLine()!)!
