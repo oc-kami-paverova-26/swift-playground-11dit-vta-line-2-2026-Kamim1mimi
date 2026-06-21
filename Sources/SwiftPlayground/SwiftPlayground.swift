600b225e73e526a4de1b7ad802a5fc0b668a1dde
oc-kami-paverova-26 <kami.paverova@student.onslow.school.nz>
2026-06-21 22:17:19 +0000

    start

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,53 +4,51 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        let birds: [String] = ["Tieke", "Kākā", "Takahē", "Hihi", "Kiwi", "Pāteke", "Tūī", "Kererū"]
-        let insects: [String] = ["Giant Wētā", "Tree Wētā", "Cave Wētā", "Putoko", "Pūngāwerewere", "Ngaokeoke", "Waemano", "Kapowai"]
-        var animalType = "none"
-        var animalNumber = 0
+        /*goal:
+        ask for sleep = done
+        calculate the hours of each day's sleep
+        calculate how much they're together
+        calculate the average
+        calculate if they're getting enough sleep
+        tell the result
+        additional:
+            check if the input is correct and makes sense
+        */
+        /*//7 days for if they went to bed at am or pm
+        var bedTime: [String] = ["none", "none", "none", "none", "none", "none", "none"]*/
         var loop = true
+        var bedTime: Double = 0
+        var wakeTime: Double = 0
+        var userInput: Double? = 0
+        //temporary variable that will be put into the above variables
+        var daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        var timeSlept: [Double] = []
 
-        var animalsSeen: [String] = [""] 
-
-        print("Welcome to Zealandia Tracker.")
-        print("Did you see a bird or an insect")
-        while loop == true {
-            animalType = readLine()!.lowercased()
-            if animalType == "bird" {
-                loop = false
-                for (a, b) in birds.enumerated() {
-                    print(a, b)
-                    }
-                print("Enter a number.")
-                loop = true
-                while loop == trssue {
-                if let animalNumber = Int(readLine()!) {
-                    animalsSeen.append(birds[animalNumber])
-                } else {
-                    print("Please enter a valid integer.")
-                    }
-                }
-
-
-
-
-            if animalType == "insect" {
-                loop = false
-                for (a, b) in insects.enumerated() {
-                    print(a, b)
-                }
+        daysOfTheWeek.forEach { day in
+            while loop == true {
+                print("When did you go to bed (pm) on \(day) (Round to the nearest hour.)")
+                userInput = Double(readLine()!)!
+                checkTime()
             }
-            else {
-                print("Please enter a 'bird' or an 'insect'.")
-            }
+            
+            print("When did you wake up (am) on \(day) (Round to the nearest hour)")
+            wakeTime = Double(readLine()!)!
+            timeSlept.append(20 - bedTime + wakeTime)
+            print("You slept for \(timeSlept)")
         }
 
+func checkTime() {
+    //checks if evening input makes sense
+    if userInput! >= 0 || userInput! <= 24 {
+        loop = false
+        bedTime = userInput!
 
-        print("Enter a number between 1–9")
+    } else {
+        print("Please enter a valid integer.")
+    }
+
+}
+
+}
         
-        let userInput: Int = Int(readLine()!)!
-        if userInput >= userInput {}
-        }
-
-        }
-        
+}
