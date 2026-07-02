aa09231e3b7448b952b2e3b7e63ba14175dde419
oc-kami-paverova-26 <kami.paverova@student.onslow.school.nz>
2026-07-02 03:07:10 +0000

    finished

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -3,12 +3,12 @@
 
 @main
 struct SwiftPlayground {
-    //No crashing at wrong data
     static func main() {
     var loop = true
     var userInput = 5
     var hourLimit: Int = 0
     var screenTimeToday: Int = 0
+    let hoursInTheDay = 24
     var screenTimeAllWeek: Int = 0
     let daysOfTheWeek: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     let apps : [String] = ["TikTok", "Youtube", "Facebook", "Instagram", "Snapchat", "Other"]
@@ -18,13 +18,31 @@ struct SwiftPlayground {
     print("What is the hour limit set for each day?")
     print("Enter a number:")
 
-    //if the time limit is not possible in real life, it will repeat
-    // until they enter a valid number. 
+    //if the time limit is above the hours in the day, or below 0,
+    //it will repeat until they enter a valid number. 
+    
+    //the loop is there so if they enter an invalid number
+    // they won't be able to continue until they enter a valid number.
     loop = true
-    hourLimit = Int(readLine()!)!
-
+    while loop == true {
+    if let hourLimitInput = Int(readLine()!) {
+        if hourLimitInput > hoursInTheDay || hourLimitInput < 0 {
+            print("Please enter a valid number.")
+        } 
+        else {
+            hourLimit = hourLimitInput
+            loop = false
+        }
+    }
+    else {
+        print("Please enter a valid number.")
+    }
+    }
+    
+    //for every day of the week it will gather data.
     daysOfTheWeek.forEach { day in
         print("On \(day) how many hours did you spend on:")
+        //for every app it will gather data.
         apps.forEach { app in
             //turns on the loop
             loop = true
@@ -39,11 +57,11 @@ struct SwiftPlayground {
                     //repeat the loop until they add a possible input.
                     //It finds out how much more time of the day is left
                     //and compares if the input is bigger than it. 
-                    if userInput > 24 - screenTimeToday {
+                    if userInput > hoursInTheDay - screenTimeToday {
                         print("Please enter a possible time.")
-                    } else if userInput >= 0 && userInput <= 24 {
-                    screenTimeToday = screenTimeToday + userInput
-                    loop = false
+                    } else if userInput >= 0 && userInput <= hoursInTheDay {
+                        screenTimeToday = screenTimeToday + userInput
+                        loop = false
                     }
                     else {
                         print("Please enter a valid number.")
@@ -58,45 +76,39 @@ struct SwiftPlayground {
                 }
 
         print("Your screen time today is \(screenTimeToday).")
+        //If the screen time is above their hour limit:
         if screenTimeToday > hourLimit {
             print("You're above your screen time limit. You should be more careful.")
         }
-
-        if screenTimeToday == hourLimit {
+        //if the screen time is the same as their hour limit:
+        else if screenTimeToday == hourLimit {
             print("Your screen time is exactly as your screen time limit.")
         }
-
-        if screenTimeToday < hourLimit {
+        //if the screen time is below their hour limit:
+        else if screenTimeToday < hourLimit {
             print("Good job! Your screen time is below the hour limit.")
         }
+        //adds how much they were on their screen today to the whole week
+        // so it can later calculate the average.
         screenTimeAllWeek = screenTimeAllWeek + screenTimeToday
+        //resets how much screen time they had today.
         screenTimeToday = 0
         }
 
+    //prints out the user's amount of and average of screen time. 
     print("This week you spent \(screenTimeAllWeek) hours on your screen, which is an average of \(screenTimeAllWeek / daysOfTheWeek.count) hours.")
-    if screenTimeAllWeek / daysOfTheWeek.count == hourLimit {
-        print("Hello")}
+    //if the user's screen time is above the hour limit.
     if screenTimeAllWeek / daysOfTheWeek.count > hourLimit {
             print("That is ABOVE the limit set for this week.")
         }
+    //if the user's screen time is the same as the hour limit.
     if screenTimeAllWeek / daysOfTheWeek.count == hourLimit {
             print("That is perfectly the limit set for this week.")
         }
+    //if the user's screen time is below the hour limit.
     if screenTimeAllWeek / daysOfTheWeek.count < hourLimit {
             print("That is below the limit set for this week. Good job.")
         }
     screenTimeAllWeek = screenTimeAllWeek + screenTimeToday
     }
 }
-
-func inputTesting(prompt: String, from: Int, to: Int) -> Int {
-    while true {
-        print(prompt, terminator: " ")
-        if let userInput = readLine(), let int = Int(userInput), (from...to).contains(int) {
-            return int
-        } else {
-            print("You must enter a valid whole number (integer) from \(from) to \(to).")
-            }
-        }
-    }
-
